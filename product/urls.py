adb1e3ad3619fef67c60c5ec30bc636e4e17e664
tilek999 <58734791+tilek999@users.noreply.github.com>
2020-07-11 19:24:46 +0600

    product

--- product/urls.py
+++ product/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from product.views import *
 
-urlpatterns = []
+urlpatterns = [
+    path("all/", products, name="products"),
+    path("<int:id>/", product, name="product"),
+    ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from product.models import *
 
 # Create your views here.
+def products(request):
+    context = {}
+    context["products"] = Product.objects.filter(avialable=True)
+    return render(request, "product/products.html", context)
+
+
+def product(request, id):
+    context = {}
+    context["product"] = Product.objects.get(id=id)
+    return render(request, "product/product.html", context)
